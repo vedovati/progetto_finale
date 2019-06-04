f3260bce985afc78d2127aca1d58561256bd822d
rota14139 <45587731+rota14139@users.noreply.github.com>
2019-06-04 09:36:29 +0200

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE medicinale (
-  id_medicinale int(11) NOT NULL AUTO_INCREMENT,
-  nome varchar(50) NOT NULL,
+  id_medicinale int NOT NULL AUTO_INCREMENT,
+  nome varchar(50) NOT NULL unique,
   descrizione TEXT NOT NULL,
   foglietto_illustrativo TEXT NOT NULL,
   img_path varchar(50) NOT NULL,
